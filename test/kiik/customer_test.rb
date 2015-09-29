f076acce822a42c627251dc7fb5501bbf397bb7b
Esdras Eduardo <esdrasedu@gmail.com>
2015-09-29 17:36:08 -0300

    resource and utils

--- test/kiik/customer_test.rb
+++ test/kiik/customer_test.rb
@@ -2,5 +2,9 @@ require File.expand_path('../../test_helper', __FILE__)
 
 module Kiik
   class CustomerTest < Test::Unit::TestCase
+    should "create should return a new customer" do
+      @mock.expects(:post).once.returns(make_response(make_customer))
+      assert_equal "c_test_customer", Kiik::Customer.create().id
+    end
   end
 end
