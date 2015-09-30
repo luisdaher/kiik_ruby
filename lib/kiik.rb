96f3ab2ac85baaf25dc87eeec983e2a5ba783798
Esdras Eduardo <esdrasedu@gmail.com>
2015-09-30 18:34:35 -0300

    issue #5

--- lib/kiik.rb
+++ lib/kiik.rb
@@ -1,11 +1,20 @@
+require 'yaml'
+
 require 'kiik/version'
 require 'kiik/util'
+
+require 'kiik/resource'
+require 'kiik/rest/create'
 
 require 'kiik/card'
 require 'kiik/customer'
 require 'kiik/charge'
 
 module Kiik
-  @api_base = 'https://api.kiik.com'
+  path = File.expand_path('../../config/payos.yml', __FILE__)
+  config = YAML.load_file(path)
+  env = ENV['PAYOS_ENV'] || 'development'
 
+  @host = config[env]['host']
+  @version = config[env]['version']
 end
