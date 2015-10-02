61904dd70ccf6ec29d29ba94c1cc4b4923ea2081
Esdras Eduardo <esdrasedu@gmail.com>
2015-10-02 19:56:32 -0300

    create instance and valid model

--- lib/kiik/charge.rb
+++ lib/kiik/charge.rb
@@ -1,5 +1,7 @@
 module Kiik
   class Charge < Resource
     include Kiik::Rest::Create
+
+    attr_accessor :customer, :amount, :receipt_email, :destination, :description, :statement_descriptor, :status
   end
 end
--- lib/kiik/resource.rb
+++ lib/kiik/resource.rb
@@ -4,6 +4,8 @@ module Kiik
 
     attr_accessor :id, :created
 
+    @errors = []
+
     class << self
       def class_name
         self.name.split('::')[-1]
@@ -19,15 +21,25 @@ module Kiik
 
       def build(data, error = nil)
         instance = self.new(data)
-        unless error.nil?
-          instance.errors = error.errors
-        end
+        instance.set_errors(error.errors) unless error.nil?
         instance
       end
     end
 
     def initialize(attributes = {})
-      attributes.each { |name, value| self.instance_variable_set("@#{name}", value) }
+      attributes.each{ |name, value| self.instance_variable_set("@#{name}", value) }
+    end
+
+    def valid?
+      @errors.empty?
+    end
+
+    def set_errors(errors = [])
+      @errors = errors
+    end
+
+    def to_json
+      {id: @id, created: @created}
     end
   end
 end
