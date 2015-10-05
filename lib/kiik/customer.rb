037de63674341ec2b3ca5a62679727298605e3ac
Esdras Eduardo <esdrasedu@gmail.com>
2015-10-05 19:19:12 -0300

    change to_json

--- lib/kiik/customer.rb
+++ lib/kiik/customer.rb
@@ -6,19 +6,19 @@ module Kiik
 
     def initialize(attributes = {})
       super(attributes)
-      @cards = @cards.map{ |card| Card.new(card) } unless @cards.nil?
+      @cards = @cards.nil? ? [] : @cards.map{ |card| Card.new(card) }
     end
 
     def to_json
-      super.merge!(
-        phone: @phone,
-        name: @name,
-        document: @document,
-        email: @email,
-        metadata: @metadata,
-        description: @description,
-        cards: @cards
-      )
+      super.merge!(super([
+        :phone,
+        :name,
+        :document,
+        :email,
+        :metadata,
+        :description,
+        :cards
+      ]))
     end
   end
 end
