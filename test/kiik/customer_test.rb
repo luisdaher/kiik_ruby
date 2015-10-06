a1f094d5c609672a209bdaf933c0fe6968832f91
Esdras Eduardo <esdrasedu@users.noreply.github.com>
2015-10-06 15:11:10 -0300

    Test more readable
    ths @leorog

--- test/kiik/customer_test.rb
+++ test/kiik/customer_test.rb
@@ -3,7 +3,7 @@ require File.expand_path('../../test_helper', __FILE__)
 module Kiik
   class CustomerTest < Test::Unit::TestCase
 
-    should "create empty customer should raise exception" do
+    should "raise exception for create empty customer" do
       error = {type:"invalid_request_error", param:"name", message:"Param name is required"}
       make_customer({}, error)
       assert_raise KiikError do
@@ -11,14 +11,14 @@ module Kiik
       end
     end
 
-    should "create! empty customer should return invalid customer" do
+    should "return invalid customer for create! empty customer" do
       error = {type:"invalid_request_error", param:"name", message:"Param name is required"}
       make_customer({}, error)
       customer = Kiik::Customer.create!
       assert !customer.valid?
     end
 
-    should "create should return a new customer" do
+    should "return a new customer for create" do
       data = {name:"Customer test", email:"customer@email.com", phone: "11223344", document:"11122233345"}
       make_customer data
       customer = Kiik::Customer.create data
@@ -26,7 +26,7 @@ module Kiik
       assert customer.cards.empty?
     end
 
-    should "create should return a new customer with card" do
+    should "return a new customer with card for create with card" do
       card_data = {name:"Card Name", number:"1234567890", month:12, year:17}
       customer_data = {name:"Customer test", email:"customer@email.com", phone: "11223344", document:"11122233345", card:card_data}
       make_customer customer_data
@@ -35,7 +35,7 @@ module Kiik
       assert customer.cards[0].name == card_data[:name]
     end
 
-    should "create using instance should return customer" do
+    should "return customer for create using instance" do
       data = {name:"Customer test", email:"customer@email.com", phone: "11223344", document:"11122233345"}
       make_customer data
       customer = Kiik::Customer.new data
