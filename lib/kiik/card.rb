2e81d373f5de2a466a6cc8562cc8a6521c79acf9
Esdras Eduardo <esdrasedu@gmail.com>
2015-11-18 19:54:35 -0200

    remove check

--- lib/kiik/card.rb
+++ lib/kiik/card.rb
@@ -4,15 +4,5 @@ module Kiik
 
     attr_accessor :customer_id, :name, :number, :month, :year, :cvc, :card_alias
 
-    def check
-      Card.check customer_id, csv
-    end
-
-    class << self
-      def check(customer_id, cvc)
-        
-      end
-    end
-
   end
 end
