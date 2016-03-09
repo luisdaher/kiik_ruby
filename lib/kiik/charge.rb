3acf79b6fb04a3d4bd24ccf1811bcc1da4d582e9
Jonatan da Hora <jonatandahora@gmail.com>
2016-03-09 19:11:06 -0300

    added expected_compensation on charge

--- lib/kiik/charge.rb
+++ lib/kiik/charge.rb
@@ -2,6 +2,7 @@ module Kiik
   class Charge < Resource
     include Kiik::Rest::Create
 
-    attr_accessor :customer_id, :amount, :receipt_email, :destination, :description, :statement_descriptor, :status, :intermediaries
+    attr_accessor :customer_id, :amount, :receipt_email, :destination,
+    :description, :statement_descriptor, :status, :intermediaries, :expected_compensation
   end
 end
