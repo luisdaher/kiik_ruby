8f8ffeaa071d418f01f46803ec467c66e73a4ccc
Esdras Eduardo <esdrasedu@gmail.com>
2016-08-04 16:31:41 -0300

    Fix date filter

--- lib/kiik/compensation.rb
+++ lib/kiik/compensation.rb
@@ -3,19 +3,19 @@ module Kiik
     include Kiik::Rest::GetAll
 
     attr_accessor :status, :received_at, :history_type, :expected_compensation_date,
-      :compensated_at, :charge_id, :amount, :aditional_info
+      :compensated_at, :charge_id, :amount, :aditional_info, :total
 
     class << self
-      def consolidate(customer = nil, start_date = nil, end_date = nil)
-        url = customer.nil? ? '' : "/#{customer}"
-        url += "/consolidate"
+      def consolidated(customer = nil, start_date = nil, end_date = nil)
+        url = customer.nil? ? '' : "#{customer}/"
+        url += "consolidated"
         params = []
-        params << "start_date=#{start_date}" if start_date
-        params << "end_date=#{end_date}" if end_date
+        params << "start_date=#{start_date.strftime('%Y-%m-%d %H:%M:%S')}" if start_date
+        params << "end_date=#{end_date.strftime('%Y-%m-%d %H:%M:%S')}" if end_date
         url += "?#{params.join('&')}" if params.length
         result = request(url, {}, :GET, {})
         raise result if result.kind_of? StandardError
-        result
+        result.total
       end
     end
 
