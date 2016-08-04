7034f1ff3f62a7f3e940d6860e7a1772c76b8d58
Esdras Eduardo <esdrasedu@gmail.com>
2016-08-04 11:51:08 -0300

    Add compensation consolidate

--- lib/kiik/compensation.rb
+++ lib/kiik/compensation.rb
@@ -5,6 +5,20 @@ module Kiik
     attr_accessor :status, :received_at, :history_type, :expected_compensation_date,
       :compensated_at, :charge_id, :amount, :aditional_info
 
+    class << self
+      def consolidate(customer = nil, start_date = nil, end_date = nil)
+        url = "consolidate"
+        url += "/#{customer}" if customer
+        params = []
+        params << "start_date=#{start_date}" if start_date
+        params << "end_date=#{end_date}" if end_date
+        url += "?#{params.join('&')}" if params.length
+        result = request(url, {}, :GET, {})
+        raise result if result.kind_of? StandardError
+        result
+      end
+    end
+
     def initialize(attributes = {})
       super(attributes)
     end
