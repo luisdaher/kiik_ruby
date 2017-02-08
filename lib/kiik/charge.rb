d774c4d5c467c074df2e7b93cbe4e409700b019f
Luis Daher <luisotaviodaher@gmail.com>
2017-02-07 23:57:41 -0200

    changing customer_id for customer on charge

--- lib/kiik/charge.rb
+++ lib/kiik/charge.rb
@@ -2,7 +2,7 @@ module Kiik
   class Charge < Resource
     include Kiik::Rest::Create
 
-    attr_accessor :customer_id, :amount, :receipt_email, :destination,
+    attr_accessor :customer, :amount, :receipt_email, :destination,
     :description, :statement_descriptor, :status, :intermediaries, :expected_compensation
   end
 end
